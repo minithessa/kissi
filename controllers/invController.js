beaa392c91cf84bb5e669e6410a38d099bd44b07
minithessa <158075593+minithessa@users.noreply.github.com>
2024-02-01 12:50:47 +0100

    Update invController.js

--- controllers/invController.js
+++ controllers/invController.js
@@ -19,4 +19,22 @@ invCont.buildByClassificationId = async function (req, res, next) {
   })
 }
 
+
+
+/*******************************
+ * Build inventory by inv view
+ *******************************/
+invCont.buildByModelId = async function (req, res, next) {
+  const model_id = req.params.inventoryId
+  const data = await invModel.getModelById(model_id)
+  const grid = await utilities.buildModelGrid (data)
+  let nav = await utilities.getNav()
+  const vehicle = data[0].inv_year + " " + data[0].inv_make + " " + data[0].inv_model
+  res.render("./inventory/inventory.ejs", {
+      title: vehicle,
+      nav,
+      grid,
+  })
+}
+
 module.exports = invCont
