5ad7a31d8adf08435acdf73998320dea2ab66d28
minithessa <158075593+minithessa@users.noreply.github.com>
2024-02-01 12:38:40 +0100

    Update inventoryRoute.js

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,14 +2,12 @@
 const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
+const utilities = require("../utilities/")
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId))
 
-
-// Inventory routes
-app.use("/inv", inventoryRoute)
-
-
+// Route to build inventory view
+router.get("/detail/:inventoryId", utilities.handleErrors(invController.buildByModelId))
 
 module.exports = router;
