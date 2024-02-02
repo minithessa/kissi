4221bede2e1206496104ca40e55f3d48167bb6e1
minithessa <158075593+minithessa@users.noreply.github.com>
2024-02-02 03:19:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-/* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
-    ******************************************/
 /* ***********************
  * Require Statements
  *************************/
-const baseController = require("./controllers/baseController")
+ 
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
+const static= require("./routes/static")
+const baseController= require("./controllers/baseControllers")
+const inventoryRoute= require("./routes/inventoryRoute")
+const utilities= require("./utilities")
+const pool = require('./database')
+
  
 
 /* ***********************
@@ -23,11 +25,12 @@ app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Routes
  *************************/
-app.use(require("./routes/static"))
-app.get("/", baseController.buildHome)
+app.use(static)
+app.get("/", utilities.handleErrors(baseController.buildHome))
+app.use("/inv", utilities.handleErrors(inventoryRoute))
+ 
+ 
 
-// Inventory routes
- app.use("/inv", require("./routes/inventoryRoute"))
 
  // Index route
 app.get("/",function(req, res){
