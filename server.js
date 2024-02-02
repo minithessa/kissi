d43718097cc98ee0123d7d3d814ee9000fde8126
minithessa <158075593+minithessa@users.noreply.github.com>
2024-02-02 10:45:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+/* ******************************************
+ * This server.js file is the primary file of the 
+ * application. It is used to control the project.
+    ******************************************/
+/* ***********************
+ * Require Statements
+ *************************/
+const baseController = require("./controllers/baseController")
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
-const static= require("./routes/static")
-const baseController= require("./controllers/baseController")
-const inventoryRoute= require("./routes/inventoryRoute")
-const utilities= require("./utilities"/)
-const pool = require('./database/')
-
  
 
 /* ***********************
@@ -15,18 +17,17 @@ const pool = require('./database/')
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout")  
+app.set("layout", "./layouts/layout") // not at views root
 
  
 /* ***********************
  * Routes
  *************************/
 app.use(require("./routes/static"))
-app.get("/", utilities.handleErrors(baseController.buildHome))
-app.use("/inv", utilities.handleErrors(inventoryRoute))
- 
- 
+app.get("/", baseController.buildHome)
 
+// Inventory routes
+ app.use("/inv", require("./routes/inventoryRoute"))
 
  // Index route
 app.get("/",function(req, res){
