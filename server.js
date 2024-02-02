a53207e3cd083f2d4604e76c10227538a2510673
minithessa <158075593+minithessa@users.noreply.github.com>
2024-02-02 10:48:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -47,3 +47,111 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
+const env = require("dotenv").config()
+const app = express()
+const static= require("./routes/static")
+const baseController= require("./controllers/baseController")
+const inventoryRoute= require("./routes/inventoryRoute")
+const utilities= require("./utilities"/)
+const pool = require('./database/')
+
+ 
+
+/* ***********************
+ *  View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")  
+
+ 
+/* ***********************
+ * Routes
+ *************************/
+app.use(require("./routes/static"))
+app.get("/", utilities.handleErrors(baseController.buildHome))
+app.use("/inv", utilities.handleErrors(inventoryRoute))
+ 
+ 
+
+
+ // Index route
+app.get("/",function(req, res){
+  res.render("index",{title:"Home"})
+})
+
+/* ***********************
+ * Local Server Information
+ * Values from .env (environment) file
+ *************************/
+const port = process.env.PORT
+const host = process.env.HOST
+
+/* ***********************
+ * Log statement to confirm server operation
+ *************************/
+app.listen(port, () => {
+  console.log(`app listening on ${host}:${port}`)
+})
