460cfffd2a36d26439276b446d6c1a2c46bc9041
minithessa <158075593+minithessa@users.noreply.github.com>
2024-02-04 00:07:25 +0100

    Update accountRoute.js

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,8 +4,22 @@ const router = new express.Router()
 const utilities = require("../utilities")
 const accountController = require("../controllers/accountController")
 
+/* ***********************************
+ * Deliver Login View
+   ********************************** */
+
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
 
+/* *******************************************
+ * Deliver Registration View
+   ****************************************** */
+
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
+/* *************************************
+ * Process Registration
+   ************************************ */
+
+router.post('/register', utilities.handleErrors(accountController.registerAccount))
 
 module.exports = router
