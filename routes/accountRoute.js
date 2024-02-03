5b75d962359f6609d141443aa2254c6f47ed111b
minithessa <158075593+minithessa@users.noreply.github.com>
2024-02-03 21:09:37 +0100

    Update accountRoute.js

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,14 +1,9 @@
 // Needed Resources
 const express = require("express")
 const router = new express.Router()
-const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
-
-
+const accountController = require("../controllers/accountController")
 
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
-
-
-
 
 module.exports = router
