4a5436180c4baa706111eefc8fe32ff798096411
minithessa <158075593+minithessa@users.noreply.github.com>
2024-02-04 01:49:42 +0100

    Update accountRoute.js

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router()
 const utilities = require("../utilities")
 const accountController = require("../controllers/accountController")
+const regValidate = require("../utilities/account-validation");
 
 /* ***********************************
  * Deliver Login View
