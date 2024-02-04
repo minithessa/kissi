02c11ff64797be74bb44d40e43442e7cf113b1b8
minithessa <158075593+minithessa@users.noreply.github.com>
2024-02-04 04:08:44 +0100

    Update accountController.js

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -3,7 +3,7 @@
  ********************************** */
 const accountModel = require("../models/account-model");
 const utilities = require('../utilities')
-
+const bcrypt = require("bcryptjs");
 
 /* ****************************************
 *  Deliver login view
@@ -30,49 +30,142 @@ async function buildRegister (req, res, next) {
 }
 
 
-
-
 /* ****************************************
-*  Process Registration
-* *************************************** */
+ *  Process Registration
+ * *************************************** */
 async function registerAccount(req, res) {
-  let nav = await utilities.getNav()
-  const { account_firstname, account_lastname, account_email, account_password } = req.body
+  let nav = await utilities.getNav();
+  const {
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password,
+  } = req.body;
+
+  // Hash the password before storing
+  let hashedPassword;
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10);
+  } catch (error) {
+    req.flash(
+      "notice",
+      "Sorry, there was an error processing the registration.",
+    );
+    res.status(500).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    });
+  }
 
   const regResult = await accountModel.registerAccount(
     account_firstname,
     account_lastname,
     account_email,
-    account_password
-  )
+    hashedPassword,
+  );
 
   if (regResult) {
     req.flash(
       "notice",
-      `Congratulations, you\'re registered ${account_firstname}. Please log in.`
-    )
+      `Congratulations, you\'re registered ${account_firstname}. Please log in.`,
+    );
     res.status(201).render("account/login", {
       title: "Login",
       nav,
-    })
+    });
   } else {
-    req.flash("notice", "Sorry, the registration failed.")
+    req.flash("notice", "Sorry, the registration failed.");
     res.status(501).render("account/register", {
       title: "Registration",
       nav,
-    })
+    });
   }
 }
 
-
-
-
-
-
-
 module.exports = {
-   buildLogin, 
-   buildRegister,
+  buildLogin,
+  buildRegister,
   registerAccount
- 
-}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
