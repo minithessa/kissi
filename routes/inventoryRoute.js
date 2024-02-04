bd632cfa38e8c0e81b7e05e8dae462fd9a0a24ca
minithessa <158075593+minithessa@users.noreply.github.com>
2024-02-04 06:07:34 +0100

    Update inventoryRoute.js

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,13 +1,49 @@
-// Needed Resources 
-const express = require("express")
-const router = new express.Router() 
-const invController = require("../controllers/invController")
-const utilities = require("../utilities/")
+// Needed Resources
+const express = require("express");
+const router = new express.Router();
+const invController = require("../controllers/invController");
+const utilities = require("../utilities/");
+const regValidate = require("../utilities/inventory-validation");
 
-// Route to build inventory by classification view
-router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId))
+ 
+router.get(
+  "/type/:classificationId",
+  utilities.handleErrors(invController.buildByClassificationId),
+);
 
-// Route to build inventory view
-router.get("/detail/:inventoryId", utilities.handleErrors(invController.buildByModelId))
+ 
+router.get(
+  "/detail/:inventoryId",
+  utilities.handleErrors(invController.buildByModelId),
+);
+
+router.get("/", invController.buildManagement);
+
+router.get("/add-classification", invController.BuildAddClassification);
+
+/* ***********************
+ *   Add the new Classification
+   *************************/
+router.post(
+  "/add-classification",
+  regValidate.classificationRules(),
+  regValidate.checkClassificationData,
+  utilities.handleErrors(invController.AddNewClassification),
+);
+
+/* ***********************
+ * Add-Inventory View
+   *************************/
+router.get("/add-inventory", invController.BuildAddInventory);
+
+/* ***********************
+ * Add the new Inventory
+   *************************/
+router.post(
+  "/add-inventory",
+  regValidate.inventoryRules(),
+  regValidate.checkInventoryData,
+  utilities.handleErrors(invController.AddNewInventory),
+);
 
 module.exports = router;
