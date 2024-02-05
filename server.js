41327898baaae18ec6312f3f5086f8810bb9e4ce
minithessa <158075593+minithessa@users.noreply.github.com>
2024-02-05 21:53:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,208 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ******************************************
+ * This server.js file is the primary file of the 
+ * application. It is used to control the project.
+ *******************************************/
+/* ***********************
+ * Require Statements
+ *************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
-const app = express()  
-const static= require("./routes/static")
-const baseController= require("./controllers/baseController")
-const inventoryRoute= require("./routes/inventoryRoute")
+const app = express()
+const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 const accountRoute = require("./routes/accountRoute")
 const errorRoute = require("./routes/errorRoute")
-const utilities= require("./utilities/")
+const utilities = require("./utilities/")
 const session = require("express-session")
 const bodyParser = require("body-parser")
 const pool = require('./database/')
 
-
-
-
-/* ***********************
- *  View Engine and Templates
- *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts) 
-app.use(require('connect-flash')())
-app.set("layout", "./layouts/layout")  
 
 
 /* ***********************
@@ -37,55 +218,59 @@ app.use(session({
   saveUninitialized: true,
   name: 'sessionId',
 }))
-
-// Express Messages Middleware
-app.use(require('connect-flash')());
-app.use(function(req, res, next){
-  res.locals.messages = require('express-messages')(req, res);
-  next();
-});
-
-//  Process Registration 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
  *************************/
-// Index route
-//app.get("/",function(req, res){
- // res.render("index",{title:"Home"})
-//})
-
 app.use(static)
+
+//Index route
+//app.get("/", function(req,res){
+//  res.render("index", {title:"Home"})
+//})
+app.use(static);
 app.get("/", utilities.handleErrors(baseController.buildHome))
-app.use("/inv",utilities.handleErrors(inventoryRoute))
-app.use("/account",utilities.handleErrors(accountRoute))
-app.use("/error",utilities.handleErrors(errorRoute))
-app.get("/error", utilities.handleErrors(baseController.buildError))
+app.use("/inv", utilities.handleErrors(inventoryRoute))
+app.use("/account", utilities.handleErrors(accountRoute))
+app.use("/borkened", utilities.handleErrors(errorRoute))
 
+
+// File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
-  next({ status: 404, message: "Sorry, we appear to have lost that page." });
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 });
 
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
 app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav();
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
-  if (err.status == 404) {
-    message = err.message;
-  }
-  if (err.status == 500) {
-    message = err.message;
-  } else {
-    message = "Oh no! There was a crash. Maybe try different route?";
-  }
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
-    title: err.status || "Server Error",
+    title: err.status || 'Server Error',
     message,
-    nav,
-  });
-});
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
