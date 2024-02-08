3ac6f037a8845147c92f82f24e99e05e7ae517ad
minithessa <158075593+minithessa@users.noreply.github.com>
2024-02-08 18:43:45 +0100

    Update index.js

--- utilities/index.js
+++ utilities/index.js
@@ -88,67 +88,6 @@ Util.buildModelGrid = async function(data){
  **************************************** */
 Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /* ****************************************
 * Middleware to check token validity
 **************************************** */
