64d80c6d90e8561eaf01e033bcd48b5d0a2ac6ca
minithessa <158075593+minithessa@users.noreply.github.com>
2024-02-08 12:17:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const utilities= require("./utilities/")
 const session = require("express-session")
 const bodyParser = require("body-parser")
 const pool = require('./database/')
-
+const cookieParser = require("cookie-parser")
 
 
 
