39b0838990226fff8676b62121686a505838ea22
minithessa <158075593+minithessa@users.noreply.github.com>
2024-02-09 09:38:28 +0100

    Update accountController.js

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -113,6 +113,49 @@ async function registerAccount(req, res) {
   }
 }
 
+
+
+
+
+
+
+
+
+
+
+/* ****************************************
+ *  Process login request
+ * ************************************ */
+accountCont.accountLogin = async function (req, res) {
+    let nav = await utilities.getNav()
+    const { account_email, account_password } = req.body
+    const accountData = await accountModel.getAccountByEmail(account_email)
+    if (!accountData) {
+     req.flash("notice", "Please check your credentials and try again.")
+     res.status(400).render("account/login", {
+      title: "Login",
+      nav,
+      errors: null,
+      account_email,
+     })
+    return
+    }
+    try {
+     if (await bcrypt.compare(account_password, accountData.account_password)) {
+     delete accountData.account_password
+     const accessToken = jwt.sign(accountData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 1000 })
+     res.cookie("jwt", accessToken, { httpOnly: true, maxAge: 3600 * 1000 })
+     return res.redirect("/account/")
+     }
+    } catch (error) {
+     return new Error('Access Forbidden')
+    }
+   }
+
+
+
+
+
 module.exports = {
   buildLogin,
   buildRegister,
