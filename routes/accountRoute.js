d081b372afe9bbe8ccbd1d721ca0396659e1e0f1
minithessa <158075593+minithessa@users.noreply.github.com>
2024-02-17 00:20:03 +0100

    Update accountRoute.js

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -5,12 +5,15 @@ const accountController = require("../controllers/accountController")
 const regValidate = require("../utilities/account-validation");
 const utilities = require("../utilities")
 
-
 // Route to build inventory by classification view
 
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
 router.get("/", utilities.checkLogin, utilities.handleErrors(accountController.accountManagement))
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
+router.get("/addAccount",  utilities.checkLogin, utilities.checkAccountType, utilities.handleErrors(accountController.buildAddAccount));
+router.get("/getAccounts", utilities.handleErrors(accountController.getAccountsJSON));
+router.get("/update/:accountId", utilities.checkLogin, utilities.handleErrors(accountController.buildEditAccount));
+router.get("/delete/:accountId", utilities.checkLogin, utilities.checkAccountType, utilities.handleErrors(accountController.buildDeleteAccount));
 
 
 /* *************************************
@@ -32,6 +35,41 @@ router.post(
     utilities.handleErrors(accountController.accountLogin)
   )
 
+router.post(
+  "/addAccount",
+  utilities.checkLogin, 
+  utilities.checkAccountType,
+  regValidate.addAccountRules(),
+  regValidate.checkAddAccountData,
+  utilities.handleErrors(accountController.addAccount)
+);
+
+router.post(
+  "/update",
+  utilities.checkLogin, 
+  //utilities.checkAccountUpdateAccess,
+  regValidate.updateAccountRules(),
+  regValidate.checkAccountUpdateData,
+  utilities.handleErrors(accountController.updateAccount)
+);
+
+router.post(
+  "/updatePassword",
+  utilities.checkLogin, 
+  utilities.checkAccountUpdateAccess,
+  regValidate.changePasswordRules(),
+  regValidate.checkResetPasswordData,
+  utilities.handleErrors(accountController.updateAccountPassword)
+);
+
+router.post(
+  "/delete",
+  utilities.checkLogin, 
+  utilities.checkAccountType,
+  regValidate.deleteAccountRules(),
+  regValidate.checkDeleteAccountData,
+  utilities.handleErrors(accountController.deleteAccount)
+);
 
 
 module.exports = router
